0d11f6a33703c88bf094cfc094a399707ad50922
CarlosGabrielGomez <gomezkarlos1988@hotmail.com>
2025-01-13 22:20:12 -0300

    se modifica el enlace y el texto del botón

--- src/assets/Components/Card/Card.jsx
+++ src/assets/Components/Card/Card.jsx
@@ -15,8 +15,8 @@ const Card = () => {
         <Body title={"ShadowX Pro"} description={"La ShadowX Pro es una zapatilla deportiva diseñada para ofrecer un rendimiento superior, sin sacrificar estilo. Su color negro elegante y su tecnología avanzada la convierten en la opción ideal para deportistas y personas activas que buscan comodidad, durabilidad y un diseño moderno."} />
 
         <Button
-          text="Presione para más información "
-          link="https://www.youtube.com/"
+          text="Leer más"
+          link="https://www.nike.com.ar/"
         />
       </div>
     </div>
